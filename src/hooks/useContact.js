c4f6f0904c1b1e08b2da51a7d52bb0d1e0575e54
Hamza Asghar Butt <humzabutt1190@gmail.com>
2023-09-25 17:03:11 +0500

    PopUp Trigger
    All task done ,
    Button popup a screen,
    cancel button done,
    json to csv done,
    Prime react component used for UI.

--- src/hooks/useContact.js
+++ src/hooks/useContact.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import {base_url_candor, getToken} from "../utils/constants";
+import { base_url_candor, getToken } from "../utils/constants";
 // here is the example for integrating api
 // export const contactDetails = async (payload) => {
 //     const data = await axios.get(`${base_url_candor}contacts/contact-detail/${payload}`,
@@ -12,3 +12,29 @@ import {base_url_candor, getToken} from "../utils/constants";
 //     return data.data;
 // }
 
+export const contactsDownloadInCsv = async () => {
+
+    const data = await axios.post(`${base_url_candor}contacts/download-csv`, {
+        "firstName": true,
+        "lastName": true,
+        "email": true,
+        "organization": true,
+        "occupation": true,
+        "address": true,
+        "employer": true,
+        "phone": true,
+        "searchValue": "string"
+    }, {
+        headers: {
+            Authorization: `Bearer ${getToken()}`,
+        },
+    })
+        .then(res => {
+            console.log(res.data);
+            return res.data;
+        })
+        .catch(err => {
+            console.error(err)
+            return err;
+        })
+}
